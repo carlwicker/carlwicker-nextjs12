30d67f9caf35ef04ac2e60df322166782a8addf3
Carl Wicker <carlwicker@gmail.com>
2022-12-09 15:22:57 +0000

    improved headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>UX/UI/IO</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
       </Head>
       <div className="container">{isOpen && <MobileMenu />}</div>
       <div style={{ overflow: "hidden" }}>
